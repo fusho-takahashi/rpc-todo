2e0b4ece7d0b0d23535909fd4e3e6b5bcfed27d1
fusho-takahashi <zenryaku@fusho-takahashi.me>
2023-09-20 14:18:39 +0900

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.2.3
      project name = rpc-todo-frontend
      framework = angular
      framework cli = @angular/cli@16.2.0
      package manager = npm@9.8.0
      wrangler = wrangler@3.8.0
      git = 2.39.0

---
+++ rpc-todo-frontend/src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'rpc-todo-frontend' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('rpc-todo-frontend');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('rpc-todo-frontend app is running!');
+  });
+});
